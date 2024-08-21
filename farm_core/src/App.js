3142ccca93793c2a34eb0a29b5bab28235678bdb
BGM03 <iwn0180@naver.com>
2024-08-21 12:48:50 +0900

    8.21.12:48

--- farm_core/src/App.js
+++ farm_core/src/App.js
@@ -6,6 +6,7 @@ import Customer from "./pages/Customer/Customer";
 import Admin from "./pages/Admin/Admin";
 import SignIn from "./pages/Login/SignIn/SignIn";
 import SignUp from "./pages/Login/SignUp/SignUp";
+import DashBoard from "./pages/DashBoard/DashBoard";
 
 function App() {
   return (
@@ -14,13 +15,18 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/">
-          <Route index element={<Main />}></Route>
-          <Route path="Login" element={<Login />}>
-            <Route path="SignIn" element={<SignIn />}></Route>
-            <Route path="SignUp" element={<SignUp />}></Route>
+          {/* 첫화면 */}
+          <Route index element={<Main />}>
+            <Route path="Login" element={<Login />}>
+              <Route path="SignIn" element={<SignIn />}></Route>
+              <Route path="SignUp" element={<SignUp />}></Route>
+            </Route>
           </Route>
-          <Route path="Customer" element={<Customer />}></Route>
-          <Route path="Admin" element={<Admin />}></Route>
+          {/* 메인화면 */}
+          <Route path="DashBoard" element={<DashBoard />}>
+            <Route path="Customer" element={<Customer />}></Route>
+            <Route path="Admin" element={<Admin />}></Route>
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ farm_core/src/pages/DashBoard/DashBoard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./DashBoard.module.scss";
+
+function DashBoard() {
+  return <div>DashBoard</div>;
+}
+
+export default DashBoard;
