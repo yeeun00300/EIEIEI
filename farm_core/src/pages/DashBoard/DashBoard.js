b21f08f9a97436c2c236475bba757ea6cc153f75
BGM03 <iwn0180@naver.com>
2024-08-22 12:42:36 +0900

    GM.8.22.12:42

--- farm_core/src/pages/DashBoard/DashBoard.js
+++ farm_core/src/pages/DashBoard/DashBoard.js
@@ -1,8 +1,32 @@
 import React from "react";
 import styles from "./DashBoard.module.scss";
+import * as React from "react";
+import { LineChart } from "@mui/x-charts/LineChart";
+
+const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
+const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
+const xLabels = [
+  "Page A",
+  "Page B",
+  "Page C",
+  "Page D",
+  "Page E",
+  "Page F",
+  "Page G",
+];
 
 function DashBoard() {
-  return <div>DashBoard</div>;
+  return (
+    <LineChart
+      width={500}
+      height={300}
+      series={[
+        { data: pData, label: "pv" },
+        { data: uData, label: "uv" },
+      ]}
+      xAxis={[{ scaleType: "point", data: xLabels }]}
+    />
+  );
 }
 
 export default DashBoard;
