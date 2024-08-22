8b2391b5b0ebdae3e11acc93282789874b1ca473
yeeeun <mlikshoot99@gmail.com>
2024-08-22 16:17:06 +0900

    ye

--- farm_core/src/pages/layout/nav/Nav.js
+++ farm_core/src/pages/layout/nav/Nav.js
@@ -1,17 +1,48 @@
 import React from "react";
 import styles from "./Nav.module.scss";
 import Footer from "../footer/Footer";
+import { Box, ThemeProvider } from "@mui/material";
+
+import { RichTreeView } from "@mui/x-tree-view/RichTreeView";
+
+const MUI_X_PRODUCTS = [
+  {
+    id: "grid",
+    label: "Data Grid",
+    children: [
+      { id: "grid-community", label: "@mui/x-data-grid" },
+      { id: "grid-pro", label: "@mui/x-data-grid-pro" },
+      { id: "grid-premium", label: "@mui/x-data-grid-premium" },
+    ],
+  },
+  {
+    id: "pickers",
+    label: "Date and Time Pickers",
+    children: [
+      { id: "pickers-community", label: "@mui/x-date-pickers" },
+      { id: "pickers-pro", label: "@mui/x-date-pickers-pro" },
+    ],
+  },
+  {
+    id: "charts",
+    label: "Charts",
+    children: [{ id: "charts-community", label: "@mui/x-charts" }],
+  },
+  {
+    id: "tree-view",
+    label: "Tree View",
+    children: [{ id: "tree-view-community", label: "@mui/x-tree-view" }],
+  },
+];
 
 function Nav(props) {
   return (
     <div className={styles.nav}>
-      <ul className={styles.category}>
-        <li className={styles.categoryItem}>나의 목장</li>
-        <li className={styles.categoryItem}>나의 목장</li>
-        <li className={styles.categoryItem}>나의 목장</li>
-        <li className={styles.categoryItem}>나의 목장</li>
-      </ul>
-      <Footer />
+      <Box sx={{ minHeight: 352, minWidth: 250 }}>
+        <RichTreeView items={MUI_X_PRODUCTS} />
+      </Box>
+
+      {/* <Footer /> */}
     </div>
   );
 }
