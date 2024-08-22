0c95024145d58b3efdbfebe6bdc2af99283dfd5c
yooeuddeum <wnsgh882002@gmail.com>
2024-08-22 12:24:21 +0900

    JH

---
+++ farm_core/src/pages/Intro/Intro.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SignUp from "../Login/SignUp/SignUp";
+
+function Intro(props) {
+  return (
+    <div>
+      <SignUp />
+    </div>
+  );
+}
+
+export default Intro;
--- farm_core/src/pages/Login/SignUp/SignUp.js
+++ farm_core/src/pages/Login/SignUp/SignUp.js
@@ -2,7 +2,37 @@ import React from "react";
 import styles from "./SignUp.module.scss";
 
 function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div>
+      <p>회원가입</p>
+      <div>
+        이름 :
+        <input />
+      </div>
+      <div>
+        아이디 :
+        <input />
+      </div>
+      <div>
+        비밀번호 :
+        <input />
+      </div>
+      <div>
+        비밀번호 확인 :
+        <input />
+      </div>
+      <div>
+        이메일 :
+        <input />
+      </div>
+      <div>
+        주소 :
+        <input />
+      </div>
+      <button>회원가입</button>
+      <button>취소</button>
+    </div>
+  );
 }
 
 export default SignUp;
