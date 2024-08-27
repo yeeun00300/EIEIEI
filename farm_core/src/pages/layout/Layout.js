cd455b794f6056c3a7b1d55b14ee6b1b6fcf3d6f
KMK23 <kmjnh34@gmail.com>
2024-08-27 19:31:00 +0900

    123123

--- farm_core/src/pages/layout/Layout.js
+++ farm_core/src/pages/layout/Layout.js
@@ -18,7 +18,6 @@ import Admin from "../Admin/Admin";
 import Weather from "../../api/Weather/Weather";
 import MyLiveStock from "../../components/MyLiveStock/MyLiveStock";
 import Livestock from "../Community/Livestock";
-import AddLiveStock from "../../components/addLiveStock/AddLiveStock";
 
 function Layout(props) {
   const dispatch = useDispatch();
@@ -185,7 +184,6 @@ function Layout(props) {
       My_Farm01: <MyLiveStock />,
       My_Farm02: <h1>나의 목장 02</h1>,
       My_Farm03: <h1>나의 목장 03</h1>,
-      My_Farm04_Add: <AddLiveStock />,
       My_Farm_Details_Farm: <h1>목장 현황</h1>,
       My_Farm_Details_CCTV: <h1>CCTV</h1>,
       My_Farm_Details_Disease: <h1>질병 현황</h1>,
