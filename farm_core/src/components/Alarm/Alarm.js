a4331ad975b4b70263e927fa9edd2bba46667954
BGM03 <iwn0180@naver.com>
2024-08-27 09:47:20 +0900

    8.27.9:47

--- farm_core/src/components/Alarm/Alarm.js
+++ farm_core/src/components/Alarm/Alarm.js
@@ -1,23 +1,20 @@
 import React from "react";
 import styles from "./Alarm.module.scss";
 
-function Alarm({ title }) {
+function Alarm({ title, description }) {
   return (
-    <div>
-      <h1>
-        {title}
-        <span>2024-08-27 00:00:00</span>
-      </h1>
-      <h3>
-        {`< 특보 현황 >
-   ○ 폭염경보: 부산, 경상남도(양산, 창원, 김해, 밀양, 의령, 함안, 창녕, 진주, 하동, 합천, 산청, 함양)
-   ○ 폭염주의보: 울산, 경상남도(거창, 통영, 거제, 고성, 남해, 사천)
-
-   <예비특보 현황>
-   ○ 풍랑 예비특보(27일 오전(06~12시)): 남해동부바깥먼바다
-   사용 예시`}
-      </h3>
-    </div>
+    <ul className={styles.Alarm}>
+      <li>
+        <h1>{title}</h1>
+        <p>2024-08-27 00:00:00</p>
+        <h3>{description}</h3>
+      </li>
+      <li>
+        <h1>{title}</h1>
+        <p>2024-08-27 00:00:00</p>
+        <h3>{description}</h3>
+      </li>
+    </ul>
   );
 }
 
