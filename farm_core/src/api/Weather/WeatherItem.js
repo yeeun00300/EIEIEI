733137899e5d24d9b779a9b37b81b6f859902ca3
BGM03 <iwn0180@naver.com>
2024-08-29 11:12:47 +0900

    Update WeatherItem.js

--- farm_core/src/api/Weather/WeatherItem.js
+++ farm_core/src/api/Weather/WeatherItem.js
@@ -4,7 +4,6 @@ import { useSelector } from "react-redux";
 import styles from "./WeatherItem.module.scss";
 
 function WeatherItem({ weatherData }) {
-  console.log(weatherData);
   const { adminLogin } = useSelector((state) => state.loginSlice);
 
   const weatherList = weatherData.list;
@@ -14,7 +13,7 @@ function WeatherItem({ weatherData }) {
     <div className={styles.weatherItem}>
       {adminLogin ? (
         <>
-          {weatherList.map((item) => {
+          {/* {weatherList.map((item) => {
             const weatherIcon = item.weather[0].icon;
             return (
               <div>
@@ -26,19 +25,17 @@ function WeatherItem({ weatherData }) {
                 <h3>{item.dt_txt}</h3>
                 <h3>{item.weather.description}</h3>
                 <h3>온도 : {item.main.temp}℃</h3>
-                {/* <h3>최소 온도 : {item.main.temp_min}℃</h3>
-                <h3>최대 온도 : {item.main.temp_max}℃</h3> */}
                 <h3>습도 : {item.main.humidity}%</h3>
                 <h3>풍속 : {item.wind.speed}m/s</h3>
                 <h3>돌풀 : {item.wind.gust}m/s</h3>
                 <h3>풍향 : {item.wind.deg}도</h3>
               </div>
             );
-          })}
+          })} */}
         </>
       ) : (
         <>
-          {weatherList.map((item) => {
+          {/* {weatherList.map((item) => {
             const weatherIcon = item.weather[0].icon;
             return (
               <div>
@@ -52,7 +49,7 @@ function WeatherItem({ weatherData }) {
                 <h3>습도 : {item.main.humidity}%</h3>
               </div>
             );
-          })}
+          })} */}
         </>
       )}
     </div>
