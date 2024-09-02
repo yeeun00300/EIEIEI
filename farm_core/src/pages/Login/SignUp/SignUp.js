2ac431a1f91784f0744024d29f041d3115940ee1
JH <wnsgh882002@gmail.com>
2024-09-02 09:27:09 +0900

    JH

--- farm_core/src/pages/Login/SignUp/SignUp.js
+++ farm_core/src/pages/Login/SignUp/SignUp.js
@@ -1,19 +1,8 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import styles from "./SignUp.module.scss";
 import { useForm } from "react-hook-form";
-import {
-  createUserWithEmailAndPassword,
-  getAuth,
-  validatePassword,
-} from "firebase/auth";
-import {
-  addDatas,
-  checkUserIdExists,
-  db,
-  getUserAuth,
-  joinUser,
-  saveUserData,
-} from "../../../firebase";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { addDatas, checkUserIdExists, saveUserData } from "../../../firebase";
 import Address from "./../../../api/address/Address";
 import logoImg from "./../../../img/TitleLogo.png";
 import { FaRegUser, FaImage } from "react-icons/fa";
@@ -28,10 +17,12 @@ function SignUp() {
   const [values, setValues] = useState({
     username: "",
     email: "",
-    id: "",
-    password: "",
     address: "",
+    detailedAddress: "",
     farm: "",
+    birthday: "",
+    nickname: "",
+    phone: "",
   });
 
   // const [email, setEmail] = useState("");
@@ -81,99 +72,66 @@ function SignUp() {
       ...values,
       [e.target.name]: e.target.value,
     });
-
-    if (e.target.name === "password") {
-      const error = validatePassword(value);
-      setPasswordError(error);
-    }
-
-    if (name === "passwordConfirm") {
-      if (value !== values.password) {
-        setPasswordMatchError("비밀번호가 일치하지 않습니다.");
-        setPasswordMatchSuccess("");
-      } else {
-        setPasswordMatchError("");
-        setPasswordMatchSuccess("비밀번호가 일치합니다.");
-      }
-    }
-    if (name === "address") {
-      setAddress((prev) => ({
-        ...prev,
-        address: value,
-      }));
-    }
-
-    if (name === "detailedAddress") {
-      setAddress((prev) => ({
-        ...prev,
-        detailedAddress: value,
-      }));
-    }
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (!idCheck) {
-      alert("아이디 중복 확인을 해주세요.");
-      return;
-    }
-    if (passwordError) {
-      alert(`비밀번호 오류: ${passwordError}`);
-      return;
-    }
+    // if (!idCheck) {
+    //   alert("아이디 중복 확인을 해주세요.");
+    //   return;
+    // }
+    // if (passwordError) {
+    //   alert(`비밀번호 오류: ${passwordError}`);
+    //   return;
+    // }
 
-    // 비밀번호 확인 검증
-    if (passwordMatchError) {
-      alert(`비밀번호 불일치: ${passwordMatchError}`);
-      return;
-    }
+    // // 비밀번호 확인 검증
+    // if (passwordMatchError) {
+    //   alert(`비밀번호 불일치: ${passwordMatchError}`);
+    //   return;
+    // }
 
     try {
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        values.email,
-        values.password
-      );
-      const { user } = userCredential;
-
-      const userData = {
-        username: values.username,
-        email: values.email,
-        id: values.id,
-        address: { ...address },
-        farm: values.farm,
-        createdAt: new Date().toISOString(),
-      };
-      await saveUserData(user.uid, userData);
+      // const auth = getAuth();
       // const userCredential = await createUserWithEmailAndPassword(
-      //   auth,
       //   values.email,
       //   values.password
       // );
       // const { user } = userCredential;
-      // const
 
-      // const userObj = {
-      //   id: values.userid,
-      //   name: values.username,
-      //   password: values.password,
+      // const userData = {
+      //   username: values.username,
       //   email: values.email,
-      //   address: values.address,
+      //   id: values.id,
+      //   address: { ...address },
       //   farm: values.farm,
-      //   createdAt: new Date(),
-      //   uid: user.uid,
+      //   createdAt: new Date().toISOString(),
       // };
+      // await saveUserData("users", user.uid, userData);
+
+      const userObj = {
+        id: values.id,
+        name: values.username,
+        email: values.email,
+        address: address.address,
+        detailedAddress: address.detailedAddress,
+        farm: values.farm,
+        createdAt: new Date(),
+      };
+      console.log(`테스트용${userObj.detailedAddress}`);
+      await addDatas("users", userObj);
       // await setDoc(doc(db, "users", user.uid), values);
       // await joinUser(("users", user.uid), values);
-      console.log("데이터 추가 성공");
+      console.log(userObj);
       alert("회원가입에 성공했습니다.");
 
       // 회원가입 성공시 이동할 페이지
-      // navigator("/LogIn");
+      navigator("/");
       // window.location.
     } catch (error) {
       console.error(error);
+      console.error("회원가입 실패:", error);
       alert(`회원가입 실패: ${error.message}`);
     }
   };
@@ -190,10 +148,10 @@ function SignUp() {
 
   // 아이디 중복확인
   const handleIdCheck = async () => {
-    if (!values.id) {
-      setIdCheckMessage("아이디를 입력해주세요.");
-      return;
-    }
+    // if (!values.id) {
+    //   setIdCheckMessage("아이디를 입력해주세요.");
+    //   return;
+    // }
 
     const exists = await checkUserIdExists(values.id);
 
@@ -220,11 +178,13 @@ function SignUp() {
   // };
 
   const handleComplete = (data) => {
-    setAddress({
-      ...address,
-      // ...enroll_company,
+    // console.log(data);
+    setValues({
+      ...values,
+      address: data.address,
+      // detailedAddress: "", // 상세주소는 초기화
     });
-    setPopup(!popup);
+    setPopup(true);
   };
 
   // 카카오 로그인
@@ -249,6 +209,7 @@ function SignUp() {
       },
     });
   };
+  // console.log(values.address.address);
 
   return (
     <div className={styles.main}>
@@ -269,21 +230,23 @@ function SignUp() {
           ref={imgRef}
           // value={imgRef}
         />
-
-        <div className={styles.name}>
-          <div className={styles.img}>
-            <FaRegUser />
-          </div>
-          <input
-            placeholder="이름을 입력해주세요."
-            type="text"
-            name="username"
-            value={values.username}
-            // onChange={(e) => setUsername(e.target.value)}
-            onChange={handleChange}
-          />
-        </div>
         <div>
+          이름
+          <div className={styles.name}>
+            <div className={styles.img}>
+              <FaRegUser />
+            </div>
+            <input
+              placeholder="이름을 입력해주세요."
+              type="text"
+              name="username"
+              value={values.username}
+              // onChange={(e) => setUsername(e.target.value)}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        {/* <div>
           아이디
           <input
             placeholder="ID를 입력해주세요."
@@ -303,8 +266,8 @@ function SignUp() {
             중복확인
           </button>
           {idCheckMessage && <p>{idCheckMessage}</p>}
-        </div>
-        <div>
+        </div> */}
+        {/* <div>
           비밀번호
           <input
             placeholder="대소문자,숫자 조합 8~16자 이내"
@@ -333,7 +296,7 @@ function SignUp() {
           {passwordMatchSuccess && (
             <p className={styles.success}>{passwordMatchSuccess}</p>
           )}
-        </div>
+        </div> */}
         <div>
           이메일
           <input
@@ -353,16 +316,17 @@ function SignUp() {
             type="text"
             required={true}
             name="address"
-            onChange={(e) => setAddress(e.target.value)}
-            // onChange={handleInput}
+            // onChange={(e) => handleChange(e.target.value)}
+            onChange={handleChange}
             value={address.address}
           />
-          <button onClick={handleComplete}>우편번호 찾기</button>
+          <button onClick={handleComplete}>주소 찾기</button>
           <div>
             <input
               placeholder="상세주소를 입력해주세요."
               type="text"
               name="detailedAddress"
+              // onChange={(e) => handleChange(e.target.value)}
               onChange={handleChange}
               value={address.detailedAddress}
             />
@@ -372,7 +336,7 @@ function SignUp() {
           )}
         </div>
         <div>
-          농장 번호
+          축사 번호
           <input
             placeholder="축사 번호를 입력해주세요"
             type="number"
@@ -381,9 +345,8 @@ function SignUp() {
             onChange={handleChange}
           />
         </div>
-
         <div className={styles.button}>
-          <button type="submit">회원가입</button>
+          <button type="submit">완료</button>
           <button>취소</button>
         </div>
       </form>
