5e4ea313bf5f0369d7ea8bc33d4bab03aa6ef2af
BGM03 <iwn0180@naver.com>
2024-09-04 10:11:05 +0900

    9.4.10:11

--- farm_core/src/api/Weather/WeatherItem.js
+++ farm_core/src/api/Weather/WeatherItem.js
@@ -14,15 +14,13 @@ import styles from "./WeatherItem.module.scss";
 
 function WeatherItem({ weatherData }) {
   const { adminLogin } = useSelector((state) => state.loginSlice);
-
   const weatherList = weatherData.list;
-
   return (
     <div className={styles.weatherItem}>
       {adminLogin ? (
         <>
           {weatherList.map((item) => {
-            const weatherIcon = item.weather[0].icon;
+            const weatherIcon = item.weather?.[0]?.icon;
             return (
               <div>
                 <h3>
@@ -30,13 +28,13 @@ function WeatherItem({ weatherData }) {
                     src={`https://openweathermap.org/img/wn/${weatherIcon}@2x.png`}
                   />
                 </h3>
-                <h3>{item.dt_txt}</h3>
-                <h3>{item.weather.description}</h3>
-                <h3>온도 : {item.main.temp}℃</h3>
-                <h3>습도 : {item.main.humidity}%</h3>
-                <h3>풍속 : {item.wind.speed}m/s</h3>
-                <h3>돌풀 : {item.wind.gust}m/s</h3>
-                <h3>풍향 : {item.wind.deg}도</h3>
+                <h3>{item?.dt_txt}</h3>
+                <h3>{item?.weather?.description}</h3>
+                <h3>온도 : {item?.main?.temp}℃</h3>
+                <h3>습도 : {item?.main?.humidity}%</h3>
+                <h3>풍속 : {item?.wind?.speed}m/s</h3>
+                <h3>돌풀 : {item?.wind?.gust}m/s</h3>
+                <h3>풍향 : {item?.wind?.deg}도</h3>
               </div>
             );
           })}
@@ -44,7 +42,7 @@ function WeatherItem({ weatherData }) {
       ) : (
         <>
           {weatherList.map((item) => {
-            const weatherIcon = item.weather[0].icon;
+            const weatherIcon = item.weather?.[0]?.icon;
             return (
               <div>
                 <h3>
@@ -52,9 +50,9 @@ function WeatherItem({ weatherData }) {
                     src={`https://openweathermap.org/img/wn/${weatherIcon}@2x.png`}
                   />
                 </h3>
-                <h3>{item.weather.description}</h3>
-                <h3>온도 : {item.main.temp}℃</h3>
-                <h3>습도 : {item.main.humidity}%</h3>
+                <h3>{item?.weather?.description}</h3>
+                <h3>온도 : {item?.main?.temp}℃</h3>
+                <h3>습도 : {item?.main?.humidity}%</h3>
               </div>
             );
           })}
--- farm_core/src/components/Admin-Weather/WeatherIssue.js
+++ farm_core/src/components/Admin-Weather/WeatherIssue.js
@@ -7,29 +7,32 @@ function WeatherIssue() {
   const [weatherIssueContent, setWeatherIssueContent] = useState([]);
   const [search, setSearch] = useState("");
   const now = new Date();
-  // const today = now.toISOString;
+  const today = now.toISOString("kr").split("T")[0].replaceAll("-", "");
+  const beforeDay2 =
+    now.toISOString("kr").split("T")[0].replaceAll("-", "") - 2;
+
   const apiKey =
     "3enTQKFbdwp7mY5McRmHelO8xxgi4LDBLefpQOsKT06WUGR3F4IhllVUPd90RuALzzzNTQuQfCGvK70tMyjJVA%3D%3D";
   const apiKey1 =
     "3enTQKFbdwp7mY5McRmHelO8xxgi4LDBLefpQOsKT06WUGR3F4IhllVUPd90RuALzzzNTQuQfCGvK70tMyjJVA==";
   const apiKey2 = "i9aTpajSSUyWk6Wo0hlMnw"; //기상청
-  const getWeatherList = async (lat, lon) => {
-    await fetch(
-      // `http://apis.data.go.kr/1360000/WthrWrnInfoService/getWthrWrnList?serviceKey=${apiKey}&numOfRows=10&pageNo=1&dataType=JSON&stnId=108&fromTmFc=20240822&toTmFc=20240826`
-      `http://apis.data.go.kr/1360000/WthrWrnInfoService/getWthrWrnList?serviceKey=${apiKey}&numOfRows=10&pageNo=1&dataType=JSON&stnId=108&fromTmFc=20240828&toTmFc=20240830`
-    )
-      .then((response) => {
-        // console.log(response);
-        return response.json();
-      })
-      .then((json) => {
-        // console.log(json);
-        setWeatherIssueList(json.response.body.items.item);
-      });
-  };
+  // const getWeatherList = async (lat, lon) => {
+  //   await fetch(
+  //     // `http://apis.data.go.kr/1360000/WthrWrnInfoService/getWthrWrnList?serviceKey=${apiKey}&numOfRows=10&pageNo=1&dataType=JSON&stnId=108&fromTmFc=20240822&toTmFc=20240826`
+  //     `http://apis.data.go.kr/1360000/WthrWrnInfoService/getWthrWrnList?serviceKey=${apiKey}&numOfRows=10&pageNo=1&dataType=JSON&stnId=108&fromTmFc=${beforeDay2}&toTmFc=${today}`
+  //   )
+  //     .then((response) => {
+  //       // console.log(response);
+  //       return response.json();
+  //     })
+  //     .then((json) => {
+  //       // console.log(json);
+  //       setWeatherIssueList(json.response.body.items.item);
+  //     });
+  // };
   const getWeatherContent = async (lat, lon) => {
     await fetch(
-      `http://apis.data.go.kr/1360000/WthrWrnInfoService/getWthrWrnMsg?serviceKey=${apiKey}&numOfRows=100&pageNo=1&dataType=JSON&stnId=108&fromTmFc=20240828&toTmFc=20240830`
+      `http://apis.data.go.kr/1360000/WthrWrnInfoService/getWthrWrnMsg?serviceKey=${apiKey}&numOfRows=100&pageNo=1&dataType=JSON&stnId=108&fromTmFc=${beforeDay2}&toTmFc=${today}`
     )
       .then((response) => {
         // console.log(response);
@@ -41,34 +44,31 @@ function WeatherIssue() {
       });
   };
   useEffect(() => {
-    getWeatherList();
+    // getWeatherList();
     getWeatherContent();
   }, []);
-  //   console.log(weatherIssueList);
-  console.log(weatherIssueContent);
+  // console.log(weatherIssueList);
 
   return (
     <div className={styles.WeatherIssue}>
       <Search setSearch={setSearch} />
       {weatherIssueContent.map((item, idx) => {
-        // console.log(item);
-        const { t1, t2, t6, tmFc } = item;
-        const alertTime1 = new Date(tmFc).toISOString("kr").split("T")[0];
-        const alertTime2 = new Date(tmFc)
-          .toISOString("kr")
-          .split("T")[1]
-          .split(".")[0];
+        console.log(item);
+
+        const { t1, t2, t6, tmFc, other } = item;
+        const year = `${tmFc}`.substring(0, 4);
+        const month = `${tmFc}`.substring(4, 6);
+        const day = `${tmFc}`.substring(6, 8);
+        const hour = `${tmFc}`.substring(8, 10);
+        const minute = `${tmFc}`.substring(10);
         return (
           <ul key={idx}>
             <li>
               <h2>
-                {t1}{" "}
-                <span>
-                  {alertTime1} {alertTime2}
-                </span>
+                {t1} <span>{`${year}-${month}-${day} ${hour}:${minute}`}</span>
               </h2>
-
               <h4>{t2}</h4>
+              <h4>{other}</h4>
             </li>
           </ul>
         );
