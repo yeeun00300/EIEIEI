1beddc7de2d3abc59a1bda877c461009d970bf0d
KMK23 <kmjnh34@gmail.com>
2024-09-04 17:05:37 +0900

    Update PaymentPage.js

--- farm_core/src/pages/paymentPage/PaymentPage.js
+++ farm_core/src/pages/paymentPage/PaymentPage.js
@@ -1,22 +1,15 @@
-import { channel } from "diagnostics_channel";
 import React from "react";
 
+const data = {
+  storeId: "store-88882013-866e-434f-8999-48de5e264888",
+  paymentId: "paymentId_202305231417002",
+  channelKey: "channel-key-7bf3b7dc-ff6a-77fc-b999-b3cb702e2663",
+};
+
 function PaymentPage(props) {
-  const requestPay = () => {
-    PortOne.requestPayment({
-      storeId: "store-8ead5501-fb96-4f25-a67c-2c9f4d8fed3a",
-      paymentId: "testm0njbmb3",
-      orderName: "EIEIEI",
-      totalAmount: 1,
-      currency: "KRW",
-      channelKey: "channel-key-89835669-045b-4fbb-8108-c19b87217568",
-      productType: "REAL",
-      payMethod: "CARD",
-    });
-  };
   return (
     <div>
-      <button onClick={requestPay}>결제</button>
+      <button>결제</button>
     </div>
   );
 }
