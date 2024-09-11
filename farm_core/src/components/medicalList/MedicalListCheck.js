ed54a4518a6f246c7eef07186b2e7f041c64e420
JH <wnsgh882002@gmail.com>
2024-09-11 17:29:47 +0900

    .

--- farm_core/src/components/medicalList/MedicalListCheck.js
+++ farm_core/src/components/medicalList/MedicalListCheck.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-function MedicalListCheck(props) {
-  return <div>ddddd</div>;
+function MedicalListCheck({ email, date }) {
+  const navigate = useNavigate();
+
+  const formData = useSelector((state) => state.medical);
+  const goDetail = () => {
+    navigate(`/medicallist/${email}`);
+  };
+  return (
+    <div>
+      <div>
+        <p>{email}</p>
+        {/* <p>{farm}</p> */}
+      </div>
+    </div>
+  );
 }
 
 export default MedicalListCheck;
