eaf36a93371f53d2e5b8a2eeeb756e182731b8f2
BGM03 <iwn0180@naver.com>
2024-09-12 16:06:37 +0900

    9.12.16:06

--- farm_core/src/components/Chatting/ChatRoom.js
+++ farm_core/src/components/Chatting/ChatRoom.js
@@ -1,8 +1,24 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Avatar, styled } from "@mui/material";
 import Badge from "@mui/material/Badge";
-
-function ChatRoom({ message, setChatRoomName, chatRoomName }) {
+import Stack from "@mui/material/Stack";
+import * as FaIcons from "react-icons/fa";
+import ChatMessage from "./ChatMessage";
+import styles from "./ChatRoom.module.scss";
+import { addMessage, getDatas, getUserAuth } from "../../firebase";
+import { serverTimestamp } from "firebase/firestore";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchChattingMessage } from "../../store/chattingSlice/chattingSlice";
+import someone from "../../img/person.png";
+function ChatRoom({ chattingRoom }) {
+  const dispatch = useDispatch();
+  const auth = getUserAuth();
+  const now = serverTimestamp();
+  const [inputValue, setInputValue] = useState("");
+  const [chatRoomName, setChatRoomName] = useState("");
+  const [photoUrl, setPhotoUrl] = useState("");
+  const { messages } = useSelector((state) => state.chattingSlice);
+  const { uid, email } = auth?.currentUser;
   const StyledBadge = styled(Badge)(({ theme }) => ({
     "& .MuiBadge-badge": {
       backgroundColor: "#44b700",
@@ -34,31 +50,95 @@ function ChatRoom({ message, setChatRoomName, chatRoomName }) {
       },
     },
   }));
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    const queryOptions = {
+      conditions: [{ field: "email", operator: "==", value: email }],
+    };
+    const addObj = {
+      text: inputValue,
+      createdAt: now,
+      uid: uid,
+    };
+    addMessage("chatting", email, chatRoomName, addObj);
+    setInputValue("");
+  };
+
+  useEffect(() => {
+    dispatch(
+      fetchChattingMessage({
+        collectionName: "chatting",
+        docId: email,
+        subCollectionName: chatRoomName,
+      })
+    );
+  }, [chatRoomName]);
   return (
     <>
-      {message.name !== "me" ? (
-        <StyledBadge
-          overlap="circular"
-          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-          variant="dot"
-        >
-          <button
-            onClick={(e) =>
-              chatRoomName !== e.target.alt
-                ? setChatRoomName(e.target.alt)
-                : setChatRoomName("")
+      <nav>
+        {chattingRoom.chattingRoom?.map((item, idx) => {
+          const { userName, photoUrl } = item;
+          return (
+            <Stack direction="row" spacing={1} key={idx}>
+              <StyledBadge
+                overlap="circular"
+                anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+                variant="dot"
+              >
+                <button
+                  onClick={(e) => {
+                    chatRoomName !== e.target.alt
+                      ? setChatRoomName(e.target.alt)
+                      : setChatRoomName("");
+                  }}
+                >
+                  <Avatar
+                    alt={userName}
+                    src={photoUrl}
+                    sx={{ width: 56, height: 56 }}
+                  />
+                </button>
+              </StyledBadge>
+            </Stack>
+          );
+        })}
+      </nav>
+      <section>
+        <main className={styles.message}>
+          {messages?.map((item, idx) => {
+            if (item.uid === uid) {
+              return (
+                <ChatMessage
+                  message={item}
+                  chatRoomName={chatRoomName}
+                  // photoUrl={someone}
+                  send
+                  key={idx}
+                />
+              );
+            } else {
+              return (
+                <ChatMessage
+                  message={item}
+                  // photoUrl={photoUrl}
+                  chatRoomName={chatRoomName}
+                  key={idx}
+                />
+              );
             }
-          >
-            <Avatar
-              alt={message.name}
-              src={message.img}
-              sx={{ width: 56, height: 56 }}
-            />
+          })}
+        </main>
+        <form onSubmit={sendMessage}>
+          <input
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+          />
+          <button disabled={!inputValue} type="submit">
+            <FaIcons.FaPaperPlane />
           </button>
-        </StyledBadge>
-      ) : (
-        <></>
-      )}
+        </form>
+      </section>
     </>
   );
 }
