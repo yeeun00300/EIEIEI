acb3106fe592bba933acba0b7fec774676fab961
BGM03 <iwn0180@naver.com>
2024-09-12 10:30:03 +0900

    9.12.10:30

--- farm_core/src/components/Chatting/Chatting.js
+++ farm_core/src/components/Chatting/Chatting.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { getUserAuth } from "../../firebase";
+import React, { useEffect, useState } from "react";
+import { addDatas, getDatas, getUserAuth } from "../../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import styles from "./Chatting.module.scss";
 import ChatRoom from "./ChatRoom";
@@ -13,17 +13,46 @@ import Karina2 from "../../img/Karina2.jpg";
 import someone from "../../img/person.png";
 import * as FaIcons from "react-icons/fa";
 import ChatMessage from "./ChatMessage";
+import { serverTimestamp } from "firebase/firestore";
 
 function Chatting() {
   const auth = getUserAuth();
   const [user] = useAuthState(auth);
   const [chatRoomName, setChatRoomName] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const now = serverTimestamp();
 
   const chatBadge = [
     { name: "me", img: someone, message: "안녕?" },
     { name: "Karina", img: Karina1, message: "누구세요", send: "received" },
     { name: "JiMin", img: Karina2, message: "누구?", send: "received" },
   ];
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    // 저장할 데이터 객체를 생성한다. {text, createdAt, photoUrl, uid}
+    const { uid, email } = auth?.currentUser;
+    const queryOptions = {
+      conditions: [{ field: "email", operator: "==", value: email }],
+    };
+    const { profileImages } = (await getDatas("users", queryOptions))[0];
+    const addObj = {
+      text: inputValue,
+      createdAt: now,
+      photoUrl: profileImages,
+      uid: uid,
+    };
+    // 데이터베이스에 객체를 저장한다.
+    addDatas(["chatting", `${email}`, `${now}`], addObj);
+
+    // inputValue 를 빈 문자열로 셋팅한다.
+    setInputValue("");
+  };
+  // const handleLoad = async () => {
+
+  // };
+  useEffect(() => {
+    // handleLoad();
+  }, []);
 
   return (
     <div className={styles.Chatting}>
@@ -47,9 +76,12 @@ function Chatting() {
             }
           })}
         </main>
-        <form>
-          <input />
-          <button>
+        <form onSubmit={sendMessage}>
+          <input
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+          />
+          <button disabled={!inputValue} type="submit">
             <FaIcons.FaPaperPlane />
           </button>
         </form>
