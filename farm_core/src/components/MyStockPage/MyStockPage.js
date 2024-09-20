293833e37ac11d2eb029916d18c14a73e608285b
yeeeun <mlikshoot99@gmail.com>
2024-09-20 16:28:28 +0900

    Update MyStockPage.js

--- farm_core/src/components/MyStockPage/MyStockPage.js
+++ farm_core/src/components/MyStockPage/MyStockPage.js
@@ -8,31 +8,33 @@ import MedicalListSave from "../medicalList/MedicalListSave";
 
 function MyStockPage(props) {
   return (
-    <div className={styles.container}>
-      <Tabs
-        defaultActiveKey="profile"
-        id="justify-tab-example"
-        className="mb-3"
-        justify
-      >
-        <Tab eventKey="four" title="축사 정보">
-          Tab content for Home
-        </Tab>
-        <Tab eventKey="five" title="축사 제어">
-          Tab content for Home
-        </Tab>
-        <Tab eventKey="one" title="문진표 작성">
-          <div className={styles.one}>
-            <MedicalList />
-          </div>
-        </Tab>
-        <Tab eventKey="two" title="문진표 내역">
-          <MedicalListSave />
-        </Tab>
-        <Tab eventKey="six" title="질병 정보">
-          Tab content for Home
-        </Tab>
-      </Tabs>
+    <div className="page">
+      <div className={styles.container}>
+        <Tabs
+          defaultActiveKey="profile"
+          id="justify-tab-example"
+          className="mb-3"
+          justify
+        >
+          <Tab eventKey="four" title="축사 정보">
+            Tab content for Home
+          </Tab>
+          <Tab eventKey="five" title="축사 제어">
+            Tab content for Home
+          </Tab>
+          <Tab eventKey="one" title="문진표 작성">
+            <div className={styles.one}>
+              <MedicalList />
+            </div>
+          </Tab>
+          <Tab eventKey="two" title="문진표 내역">
+            <MedicalListSave />
+          </Tab>
+          <Tab eventKey="six" title="질병 정보">
+            Tab content for Home
+          </Tab>
+        </Tabs>
+      </div>
     </div>
   );
 }
