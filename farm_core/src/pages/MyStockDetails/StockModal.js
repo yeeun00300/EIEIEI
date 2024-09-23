5335c5700a9ff46cbc4f231bb3b23acbfa268fd9
KMK23 <kmjnh34@gmail.com>
2024-09-23 22:09:38 +0900

    123

--- farm_core/src/pages/MyStockDetails/StockModal.js
+++ farm_core/src/pages/MyStockDetails/StockModal.js
@@ -1,16 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { updateStock } from "../../store/stockSlice/stockSlice";
+import {
+  fetchSelectedStock,
+  updateStock,
+} from "../../store/stockSlice/stockSlice";
 import Modal from "./Modal";
 
 function StockModal({ onClose }) {
   const dispatch = useDispatch();
-  const selectedStock = useSelector((state) => state.stockslice.selectedStock);
-  const [formData, setFormData] = useState(selectedStock);
+  const selectedStock = useSelector((state) => state.stockslice?.selectedStock);
+  console.log("선택된 가축 데이터:", selectedStock); // 로그 추가
+  const [formData, setFormData] = useState(selectedStock || {});
 
   useEffect(() => {
-    setFormData(selectedStock);
-  }, [selectedStock]);
+    if (selectedStock) {
+      setFormData(selectedStock);
+    } else if (selectedStock === null) {
+      // 선택된 가축이 없을 때 fetchSelectedStock 호출
+      const queryOptions = {
+        conditions: [
+          {
+            field: "docId",
+            operator: "==",
+            value: selectedStock.docId, // 여기는 제거해야 함
+          },
+        ],
+      };
+      dispatch(fetchSelectedStock({ collectionName: "stock", queryOptions }));
+    }
+  }, [dispatch, selectedStock]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -33,32 +51,31 @@ function StockModal({ onClose }) {
       <h2>가축 수정</h2>
       <input
         name="birthDate"
-        value={formData.birthDate}
+        value={formData.birthDate || ""} // 값이 없을 경우 빈 문자열로 초기화
         onChange={handleChange}
         placeholder="생일"
       />
       <input
         name="weight"
         type="number"
-        value={formData.weight}
+        value={formData.weight || ""} // 값이 없을 경우 빈 문자열로 초기화
         onChange={handleChange}
         placeholder="체중"
       />
       <input
         name="feed"
         type="number"
-        value={formData.feed}
+        value={formData.feed || ""} // 값이 없을 경우 빈 문자열로 초기화
         onChange={handleChange}
         placeholder="먹이량"
       />
       <input
         name="temp"
         type="number"
-        value={formData.temp}
+        value={formData.temp || ""} // 값이 없을 경우 빈 문자열로 초기화
         onChange={handleChange}
         placeholder="온도"
       />
-      {/* 다른 수정 가능한 필드 추가 */}
       <button onClick={handleSubmit}>저장</button>
     </Modal>
   );
