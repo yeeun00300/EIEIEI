f98308f535771d0b8eb88c3cfaf55dc0711b32ce
yeeeun <mlikshoot99@gmail.com>
2024-09-24 14:32:08 +0900

    YE

--- farm_core/src/components/MyLiveStock/charts/StockProduct.js
+++ farm_core/src/components/MyLiveStock/charts/StockProduct.js
@@ -1,7 +1,234 @@
 import React from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  PieChart,
+  Pie,
+  Cell,
+} from "recharts";
 
-function StockProduct(props) {
-  return <div>생산량</div>;
+const productData = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index,
+}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
+function StockProduct({ stock, farmData }) {
+  const { farm_stockType } = farmData;
+  const male = [];
+  const female = [];
+  stock.filter((item) => {
+    if (item.mating === "Y" && item.sex === "M") {
+      male.push(item);
+    } else if (item.mating === "Y" && item.sex === "F") {
+      female.push(item);
+    }
+  });
+
+  const data = [
+    { name: "수컷", value: male.length },
+    { name: "암컷", value: female.length },
+  ];
+
+  const stockTypeProduct = () => {
+    switch (farm_stockType) {
+      case "한우":
+        return (
+          <ResponsiveContainer width="100%" height="100%">
+            <PieChart width="100%" height="100%">
+              <Legend layout="vertical" verticalAlign="top" align="top" />
+              <Tooltip />
+              <Pie
+                data={data}
+                cx="50%"
+                cy="50%"
+                labelLine={false}
+                label={renderCustomizedLabel}
+                outerRadius={150}
+                fill="#8884d8"
+                dataKey="value"
+                style={{ outline: "none" }}
+              >
+                {data.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                    style={{ outline: "none" }}
+                  />
+                ))}
+              </Pie>
+            </PieChart>
+            <div>발정 개체수: {male.length + female.length}</div>
+          </ResponsiveContainer>
+        );
+      case "낙농":
+        return (
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart
+              width={500}
+              height={300}
+              data={productData}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey="pv"
+                stroke="#8884d8"
+                activeDot={{ r: 8 }}
+              />
+              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            </LineChart>
+          </ResponsiveContainer>
+        );
+      case "산란계":
+        return (
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart
+              width={500}
+              height={300}
+              data={productData}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey="pv"
+                stroke="#8884d8"
+                activeDot={{ r: 8 }}
+              />
+              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            </LineChart>
+          </ResponsiveContainer>
+        );
+      case "양돈":
+        return (
+          <ResponsiveContainer width="100%" height="100%">
+            <PieChart width="100%" height="100%">
+              <Legend layout="vertical" verticalAlign="top" align="top" />
+              <Tooltip />
+              <Pie
+                data={data}
+                cx="50%"
+                cy="50%"
+                labelLine={false}
+                label={renderCustomizedLabel}
+                outerRadius={150}
+                fill="#8884d8"
+                dataKey="value"
+                style={{ outline: "none" }}
+              >
+                {data.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                    style={{ outline: "none" }}
+                  />
+                ))}
+              </Pie>
+            </PieChart>
+            <div>발정 개체수: {male.length + female.length}</div>
+          </ResponsiveContainer>
+        );
+      case "육계":
+        return `닭 평균 무게`;
+      default:
+        return <div>가축 정보가 없습니다.</div>; // 기본
+    }
+  };
+  return stockTypeProduct();
 }
 
 export default StockProduct;
