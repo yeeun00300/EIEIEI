60a00de1132ac137044b74a5cbf198d733bbc444
yeeun00300 <mlikshoot99@gmail.com>
2024-09-28 11:52:39 +0900

    ye

--- farm_core/src/components/MyLiveStock/charts/StockProduct.js
+++ farm_core/src/components/MyLiveStock/charts/StockProduct.js
@@ -230,32 +230,34 @@ function StockProduct({ stock, farmData }) {
         );
       case "양돈":
         return (
-          <ResponsiveContainer width="100%" height="100%">
-            <PieChart width="100%" height="100%">
-              <Legend layout="vertical" verticalAlign="top" align="top" />
-              <Tooltip />
-              <Pie
-                data={data}
-                cx="50%"
-                cy="50%"
-                labelLine={false}
-                label={renderCustomizedLabel}
-                outerRadius={150}
-                fill="#8884d8"
-                dataKey="value"
-                style={{ outline: "none" }}
-              >
-                {data.map((entry, index) => (
-                  <Cell
-                    key={`cell-${index}`}
-                    fill={COLORS[index % COLORS.length]}
-                    style={{ outline: "none" }}
-                  />
-                ))}
-              </Pie>
-            </PieChart>
-            <div>발정 개체수: {male.length + female.length}</div>
-          </ResponsiveContainer>
+          <div className={styles.chartBox}>
+            <h5>발정 개체수: {male.length + female.length}</h5>
+            <ResponsiveContainer width="100%" height="100%">
+              <PieChart width="100%" height="100%">
+                <Legend layout="vertical" verticalAlign="top" align="top" />
+                <Tooltip />
+                <Pie
+                  data={data}
+                  cx="50%"
+                  cy="50%"
+                  labelLine={false}
+                  label={renderCustomizedLabel}
+                  outerRadius="100%"
+                  fill="#8884d8"
+                  dataKey="value"
+                  style={{ outline: "none" }}
+                >
+                  {data.map((entry, index) => (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={COLORS[index % COLORS.length]}
+                      style={{ outline: "none" }}
+                    />
+                  ))}
+                </Pie>
+              </PieChart>
+            </ResponsiveContainer>
+          </div>
         );
       case "육계":
         return (
