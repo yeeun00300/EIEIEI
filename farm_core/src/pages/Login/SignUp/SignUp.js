6f15db764cf32f200d944cd8ac60a093c853d829
KMK23 <kmjnh34@gmail.com>
2024-10-03 18:18:48 +0900

    Update SignUp.js

--- farm_core/src/pages/Login/SignUp/SignUp.js
+++ farm_core/src/pages/Login/SignUp/SignUp.js
@@ -159,10 +159,10 @@ function SignUp() {
       // `unwrap()`을 사용해 반환된 값에서 docId를 추출
       const { docId } = resultAction.payload;
       const baseUrl = process.env.REACT_APP_BASE_URL; // 환경 변수에서 URL 가져오기
-      const redirectUrl = `RegularPayment`;
+      // const redirectUrl = `RegularPayment`;
       // 회원가입 완료 후, `docId`와 함께 페이지 이동
       alert("회원가입에 성공했습니다.");
-      navigate(redirectUrl, { state: { docId, userObj } });
+      navigate("/RegularPayment", { state: { docId, userObj } });
     } catch (error) {
       alert(`회원가입 실패: ${error.message}`);
     }
